9a1029b91bbef5149bce9867f83897196b7478da
belcaid abderrahim <belcaidna@gmail.com>
2021-02-09 13:53:40 +0100

    add checkout and select product and print

--- frontend/js/filter.js
+++ frontend/js/filter.js
@@ -1,29 +1,48 @@
 
+let category_menu = document.querySelector('.category-menu');
+
+let sctgContinner = document.getElementById('sctgContinner');
 
 // get category from db 
 
-// axios.get('http://localhost:8080/category')
-// .then(function (response) {
+axios.get('http://localhost:8080/category')
+.then(function (response) {
   
-//     response.data.forEach(element => {
-//         menu_bar.innerHTML += `<li class="setting-item"><a href="#">
-//         <div class="icon">
-//             <span class="fas fa-cog"></span></div> ${element.nom} <i class="fas fa-angle-right"></i>
-//     </a></li>`
+    response.data.forEach(element => {
+        category_menu.innerHTML += `<li class="category"><a href="sousCategory.html?id=${element._id}">${element.nom}</a></li>`
     
-//     });
+    });
     
-// }).catch(function (err) {
-//     console.log(err);
-// });
+}).catch(function (err) {
+    console.log(err);
+});
+
+// --------------------get all s category --------------------
 
 
+axios.get('http://localhost:8080/sousCategory')
+.then(function (response) {
+
+    for (let i = 0; i < 8; i++) {
+        sctgContinner.innerHTML += ` <div  class="card ">
+        <h4>${response.data[i].nom}</h4>
+        <a href="#"></a>
+    </div>` 
+        
+    }
+
+   
 
 
+    
 
-
-
-
+    
+  
+   
+    
+}).catch(function (err) {
+    console.log(err);
+});
 
 
 
